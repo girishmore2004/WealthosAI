e15b0881a8ebd7741172639bc84b798c613b3cfd
Girish More <127285696+girishmore2004@users.noreply.github.com>
2026-08-13 17:42:00 +0530

    Update business.module.ts

--- apps/api/src/business/business.module.ts
+++ apps/api/src/business/business.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { BusinessController } from "./business.controller";
 import { BusinessService } from "./business.service";
+import { IncomeModule } from "../income/income.module";
 
 @Module({
+  imports: [IncomeModule],
   controllers: [BusinessController],
   providers: [BusinessService],
   exports: [BusinessService],
